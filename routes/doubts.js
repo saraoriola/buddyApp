77f322c315d15ce603048cda1b034c6f323606de
saraoriola <saraoriola@outlook.com>
2023-07-19 23:09:20 +0200

    commit -env

--- routes/doubts.js
+++ routes/doubts.js
@@ -7,32 +7,17 @@ const { authentication, isAuthor } = require('../middleware/authentication');
 router.post('/createDoubt', authentication, DoubtController.createDoubt);
 router.post('/createAnswer', authentication, DoubtController.createAnswer);
 
-router.get(
-  '/getAllDoubtsUsersAnswers',
-  DoubtController.getAllDoubtsUsersAnswers
-);
+router.get('/getAllDoubtsUsersAnswers', DoubtController.getAllDoubtsUsersAnswers);
 //router.get('/getAnswerByAnswer/answer/:answer', DoubtController.getAnswerByAnswer); EXTRA WIP
-router.get(
-  '/getAllDoubtsUsersAnswersUser',
-  DoubtController.getAllDoubtsUsersAnswersUser
-);
+router.get('/getAllDoubtsUsersAnswersUser',DoubtController.getAllDoubtsUsersAnswersUser);
 
 router.get('/getDoubtByDoubt/doubt/:doubt', DoubtController.getDoubtByDoubt);
 router.get('/getDoubtById/id/:_id', DoubtController.getDoubtById);
 //actualizar una duda ( tiene que estar autenticado)
 
-router.put(
-  '/updateDoubt/id/:_id',
-  authentication,
-  isAuthor,
-  DoubtController.updateDoubt
-);
+router.put('/updateDoubt/id/:_id',authentication,isAuthor,DoubtController.updateDoubt);
 router.put('/likes/:_id', authentication, DoubtController.like);
 
-router.delete(
-  '/deleteDoubt/id/:_id',
-  authentication,
-  DoubtController.deleteDoubt
-);
+router.delete('/deleteDoubt/id/:_id',authentication,DoubtController.deleteDoubt);
 
 module.exports = router;
